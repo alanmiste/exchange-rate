f98e6a304aca0f88756def1c76f732fc6752601b
Alan Miste <baranalaan@gmail.com>
2022-11-20 13:48:45 +0100

    Add comments to api file.

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
-
+// because the used API stopped being free
+// mock fetch method was added, which supplies
+// its own exchange rate data
 import { fetch } from "./lib/fetch";
 
+// helper to make our exchange rate API call
 export function getExchangeRates(base, supportedCurrencies) {
   const symbols = supportedCurrencies
-    .filter((symbol) => symbol !== base) 
+    .filter((symbol) => symbol !== base) // exclude your own code from requested symbols
     .join();
   const url = `http://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbols}`;
   return fetch(url)
@@ -12,7 +15,10 @@ export function getExchangeRates(base, supportedCurrencies) {
     .then((res) => res.rates);
 }
 
-
+// if there was an error log it and return super basic mock data
+// errors for this API came back in the form of { success: false, error: {...}}
+// so we can check for that and log any errors and then respond with a basic
+// valid response so the app doesn't crash
 function handleAPIErrors(res) {
   if (res.success) return res;
   console.error(`Server Error: ${res.error.info}`);
