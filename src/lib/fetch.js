98cd1fa6f7ff9c973568c77c5fdfca6399e12fc8
Alan Miste <baranalaan@gmail.com>
2022-11-20 14:04:43 +0100

    Add comments to fetch.

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,8 +1,12 @@
 import exchangeRates from "./rates.json";
 
+// sleep helper to simulate a network call
 const sleep = (time) => new Promise((res) => setTimeout(res, time));
 
-
+//since it will be a single fetch call 
+//it's going to use this mock fetch method 
+//which will simulate a short delay 
+//and return with exchange rates after a short period
 export async function fetch(url) {
   const [, search = ""] = url.split("?");
   const params = new URLSearchParams(search);
@@ -11,6 +15,7 @@ export async function fetch(url) {
   const base = params.get("base");
   const rates = exchangeRates[base];
   return {
+    // simulate the JSON method on the fetch response
     async json() {
       return {
         success: true,
