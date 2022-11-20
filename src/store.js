d9154a403cd08ff2fdcfc125a685226d35dfd0db
Alan Miste <baranalaan@gmail.com>
2022-11-20 14:18:55 +0100

    Add basic store.

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    amount: "12.00",
+    currencyCode: "USD"
+}
+function reducer(state = initialState, action){
+    return state;
+}
+
+export const store = createStore(reducer);
